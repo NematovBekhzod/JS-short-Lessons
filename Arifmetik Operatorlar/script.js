d9458ca7c23bab5ccf564f9251e1328886e39ec3
Ansor <ansoriy2001@gmail.com>
2022-05-23 08:33:17 +0500

    add file Arithmetic operations

---
+++ Arifmetik Operatorlar/script.js
@@ -0,0 +1,117 @@
+//! Arifmetik Operatorlar
+//? " + "
+//? " - "
+//? " * "
+//? " / "
+//? " ** "
+//? " % "
+//? " increment "
+//? " decrement "
+
+//-----------------------------------------------//
+
+//! " + " Qo'shish Operatori
+/*
+let a = 5
+let b = 6
+console.log(a + b); // 11 
+*/
+
+// console.log(null + undefined); // NaN
+// console.log(null + 0); // 0
+// console.log(undefined + 0); // NaN
+// console.log("si" + 7); // si7
+// console.log(null + 7); // 7
+// console.log(undefined + 7); // NaN
+
+// console.log("si" + null); // sinull
+// console.log("si" + { a: 5 }); // si[object Object]
+
+//------------------------------------------------//
+
+//! " - " Ayirish Operatori
+/*
+let a = 5
+let b = 6
+console.log(a - b); // -1 
+*/
+
+// console.log(null - undefined); // NaN
+// console.log(null - 0); // 0
+// console.log(undefined + 0); // NaN
+// console.log("si" - 7); // NaN
+// console.log(null - 7); // -7
+// console.log(undefined - 7); // NaN
+
+// console.log("si" - null); // NaN
+// console.log("si" - { a: 5 }); // NaN
+
+//-----------------------------------------------//
+
+//! " * " Ko'paytirish Operatori
+/*
+let a = 5
+let b = 6
+console.log(a * b); // 30 
+*/
+
+//------------------------------------------------//
+
+//! " / " Bo'lish Operatori
+/* 
+let a = 5
+let b = 6
+console.log(a / b); // 0.8333333333333334
+*/
+
+//------------------------------------------//
+
+//! " % " Qoldiq Chiqarish Operatori
+/*
+let a = 5
+let b = 6
+console.log(a % b); // 5
+console.log(5 % 4) // 1
+console.log(-4 % 5); // -4
+*/
+
+//--------------------------------------//
+
+//! " ** " Daraja'ga Ko'tarish Operatori
+/*
+let a = 5
+let b = 6
+console.log(a ** b); // 5
+*/
+
+//--------------------------------------//
+
+//! " ++ " Increment (Orttirish) Operatori
+/*
+let a = 5
+console.log(a++); // 5
+console.log(a); // 6
+
+let b = 9
+console.log(++a); // 10
+*/
+
+//------------------------------------//
+
+//! " -- " Decrement (Orttirish) Operatori
+/*
+let x = 5
+console.log(x--); // 5
+console.log(a); // 4
+
+let y = 9
+console.log(--a); // 8
+*/
+
+//----------------------------------//
+
+//!! " + " Qo'shish Operatori bazi xossalari
+
+//? Agar "+" ning ikkala tomonida sonlar bo'lsa Arifmetik Qo'shiladi Masalan: 7 + 8 = 15 (typeof : number)
+
+//? Agar "+" ning ikkala tomonida Son va String bo'lsa Mantiqiy Qo'shiladi Masalan: 7 + "8" = 78 (typeof : string)
