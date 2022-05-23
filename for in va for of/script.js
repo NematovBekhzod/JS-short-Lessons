c6d2cd1aa3582d657fb5f25f490136cccae698c4
Ansor <ansoriy2001@gmail.com>
2022-05-23 09:29:40 +0500

    file add (infinite loops) and (for in va for of)

---
+++ for in va for of/script.js
@@ -0,0 +1,44 @@
+//! " for in " va " for of " looplari obyekt va array bilan ishlashni osonlashtiradi
+
+//? OBJECT 'da "for in"
+
+const person = {
+  fName: "Ansor",
+  age: 20,
+};
+
+// ? for in ↙️ // => for in obyektlar bilan ishlatish oson
+
+for (let key in person) {
+  console.log(key); // key ~ property
+}
+
+for (let key in person) {
+  console.log(person[key]); // value
+}
+
+for (let key in person) {
+  console.log(key, person[key]); // property value
+}
+
+//---------------------------------------//
+
+const user = {
+  fName: "Ansor",
+  age: 20,
+};
+const keys = Object.keys(user);
+
+for (let i = 0; i < keys.length; i++) {
+  console.log(keys[i]);
+}
+
+//------------------------------------//
+
+//! Array 'da "for in"
+// ? for in ↙️ // for of arraylar bilan ishlatiladi
+
+let arr = [1, 2, 3, 4, 5];
+for (let key of arr) {
+  console.log(key); // 1 2 3 4 5
+}
---
+++ infinite loops/script.js
@@ -0,0 +1,8 @@
+"use strict";
+//! Infinite loops -- cheksiz sikl "to'xtovsiz ishlab turadi"
+
+//? for()
+//             shart : shart doim to'g'ri bo'ladi
+for(let i = 0; true; i++) {
+    console.log(i);
+} 
