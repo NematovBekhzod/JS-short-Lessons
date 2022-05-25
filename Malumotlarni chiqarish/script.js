b1d240dcf1f1c3563f9d79bbc4b15ac76377a644
Ansor <ansoriy2001@gmail.com>
2022-05-25 14:48:50 +0500

    Malumotlarni chiqarish fayli qo'shildi

---
+++ Malumotlarni chiqarish/script.js
@@ -0,0 +1,35 @@
+//! Dasturlash Turlari
+// ? 2 xil bo'ladi (typing lenguages)
+
+//* 1 - > Static Typing - (Java, C#, C++)
+//* 2 - > Dynamic Typing - (Javascript, Python, ...)
+
+//! Javascript'da Malumotlarni Chiqarish
+
+console.log("Assalomu Alaykum");
+//? console.log() funksiyasi orqali brauzer consoli'da malumotlar chiqadi
+// console.log("Assalomu Alaykum");
+
+//!    alert("Bu Alert")
+//? alert() orqali sayt yuklanishidan oldin chiqadi
+// alert("Bu Alert")
+
+//? document.write() orqali document'da malumot chiqadi
+// document.write("Hello world!!!")
+
+//! prompt() funksiyasi orqali malumotni Tastiqlash mumkin (true, false)
+prompt("Please enter");
+
+//! confirm() orqali Malumot Kiritishimiz va o'zgaruvchiga o'zlashtirib olishimiz mumkin
+let message = confirm("Please enter");
+
+/* 
+let bookName = "Imomning Maniken Qizi"
+console.log(bookName);
+
+let info = "Bu Alert"
+alert(info)
+
+let fName = "Hello My Name is Ansor"
+document.write(fName)
+*/
