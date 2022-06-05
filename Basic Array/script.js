872714eacd689c953facd46e9ddabd646606b61f
Ansor <ansoriy2001@gmail.com>
2022-06-05 09:27:23 +0500

    Basic Array add full code

--- Basic Array/script.js
+++ Basic Array/script.js
@@ -70,4 +70,82 @@ arr.lastIndexOf(); // elementlarni oxiridan boshlab qidiradi
 console.log(arr.lastIndexOf(4)); // 8
 
 arr.includes() // qidirolayotgan element arrayda mavjud bo'lsa true qaytaradi
-console.log(arr.includes(5)); // true 
+console.log(arr.includes(5)); // true
+
+
+//! Arraylarni Bilashtirish concat()
+let number = [1, 2, 3, 4];
+let arrs = [5, 6, 3, 2, 4];
+
+number.concat(arrs) // Arraylarni birlashtiradi
+let combined = number.concat(arrs)
+console.log(combined);
+
+console.log(number.concat(arr, 11));
+
+// ! Qirqib olish slice()
+//? number.slice(x, y) // birinchi parametr(x)'ga mos indexdan boshlab ikkinchi parametr(y)'ga mos bo'lgan index'gacha qirqib oladi
+// let sliced = number.slice(1, 3)
+// console.log(sliced); 
+
+//! Arraylarni clone'lash => spread[...]
+let numbers = [1, 2, 3, 4];
+let number2 = [...numbers]; // [...]
+
+console.log(number1);
+console.log(number2);
+
+
+
+let number1 = [1, 2, 3, 4, 90];
+function sum(arr) {
+    let sum = 0
+    for(let key of arr) {
+        sum+=key
+    }
+    console.log(sum);
+}
+sum(number1)
+
+function sum() {
+    let sum = 0
+    for(let key in arguments) {
+        sum+=arguments[key]
+    }
+    console.log(sum);
+}
+sum(7, 9, 1)
+/* 
+let number1 = [1, 2, 3, 4, 90];
+let sum = number1.reduce((x, y) => x + y)
+console.log(sum); */
+
+//! for in || for of
+const arrfor = [1, 2, 3, 4, 7]
+
+//? for of
+for(let key of arrfor) {
+  console.log(key); // elementlar //1 2 3 4 7
+}
+
+// ? for in
+for(let key in arrfor) {
+    console.log(key); // index'lar // 0 1 2 3 4
+}
+
+
+let numberses = [1, 2, 3, 4, 7];
+
+//! join() metodi
+// numbers.join(x)  array'ni berilgan qiymat bo'yicha ajratadi string'ga o'tkazadi 
+//let joined = numbers.join(" ") // 1 2 3 4 7
+let joined = numberses.join("-") // 1-2-3-4-7
+console.log(joined);
+
+//! split( ) metodi
+//? str.split(x) => stringni berilgan argument bo'yicha array'ga ajratadi
+let str = "Assalomu Alaykum my name is Ansor"
+let newArr = str.split(" ") // str'ni bo'sh joy bo'yicha bo'ladi
+console.log(newArr); // [ 'Assalomu', 'Alaykum', 'my', 'name', 'is', 'Ansor' ] 
+
+
